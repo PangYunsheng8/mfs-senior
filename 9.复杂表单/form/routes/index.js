7059d41c2edd59d91ead679fa62b6525af36aae8
image <337067051@qq.com>
2019-09-23 21:08:15 +0800

    add homework 9

--- 9.复杂表单/form/routes/index.js
+++ 9.复杂表单/form/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/usernameExist', function(req, res, next){
+  console.log(req.body.username)
+  res.json({
+    exist: "pangyunsheng" == req.body.username
+  })
+});
+
 module.exports = router;
